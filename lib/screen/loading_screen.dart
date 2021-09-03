8b62b99206b712f536a97d73643f4449d1d9d650
amitkum8227 <62775823+amitkum8227@users.noreply.github.com>
2021-09-03 02:23:12 -0400

    "getting response from sample Api"

--- lib/screen/loading_screen.dart
+++ lib/screen/loading_screen.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,25 +9,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
+  var url=Uri.parse('https://api.openweathermap.org/data/2.5/find?lat=55.5&lon=37.5&cnt=10&appid=6513a185e516bb6c79d470ec3c626b71');
   Future<void> getLocation() async {
-    Location location=Location();
+    Location location = Location();
     await location.getCurrentLocation();
     print(location.longitude);
-    print (location.latitude);
-
+    print(location.latitude);
   }
+
+  Future<void> getData  () async {
+    http.Response response= await  http.get(url) ;
+   if(response.statusCode==200){
+     String data= response.body;
+     print(data);
+
+   }else{
+     print(response.statusCode);
+   }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getLocation();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-
-    );
+    getData();
+    return Scaffold();
   }
 }
